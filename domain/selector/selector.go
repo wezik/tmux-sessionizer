e2965bc65b815513d1f3c954b67132621d7b75d9
Dominik <wezik@users.noreply.github.com>
2025-05-20 20:04:53 +0200

    Typo

--- domain/selector/selector.go
+++ domain/selector/selector.go
@@ -3,6 +3,6 @@ package selector
 import "phopper/domain/project"
 
 type Selector interface {
-	// if this function returns an error, treat is as a canceled selection
+	// if this function returns an error, treat it as a canceled selection
 	SelectFrom(entries []project.Project, prompt string) (project.Project, error)
 }
