cd97e0468710ce1b845667221250c1f00f449037
Dominik <wezik@users.noreply.github.com>
2025-05-24 17:28:53 +0200

    CLI implementation

--- src/domain/service/service.go
+++ src/domain/service/service.go
@@ -1,12 +1,10 @@
 package service
 
-import (
-	"phopper/src/domain/model"
-)
-
 type Service interface {
-	CreateProject(cwd, name string) (*model.Project)
+	CreateProject(cwd, name string)
 	SelectAndOpenProject(name string)
+	DeleteProject(name string)
+	EditProject(name string)
 }
 
 type ServiceImpl struct{}
@@ -15,10 +13,18 @@ func NewService() *ServiceImpl {
 	return &ServiceImpl{}
 }
 
-func (s *ServiceImpl) CreateProject(cwd, name string) (*model.Project) {
+func (s *ServiceImpl) CreateProject(cwd, name string) {
 	panic("unimplemented")
 }
 
 func (s *ServiceImpl) SelectAndOpenProject(name string) {
+	panic("unimplemented")
+}
+
+func (s *ServiceImpl) DeleteProject(name string) {
+	panic("unimplemented")
+}
+
+func (s *ServiceImpl) EditProject(name string) {
 	panic("unimplemented")
 }
--- test/app/cli/cli_test.go
+++ test/app/cli/cli_test.go
@@ -2,7 +2,6 @@ package cli_test
 
 import (
 	"phopper/src/app/cli"
-	"phopper/src/domain/model"
 	"testing"
 )
 
@@ -31,123 +30,251 @@ func (s *MockService) SelectAndOpenProject(name string) {
 	s.SelectAndOpenProjectCalls++
 }
 
-func (s *MockService) CreateProject(cwd, name string) *model.Project {
+func (s *MockService) CreateProject(cwd, name string) {
 	s.CreateProjectParam1 = cwd
 	s.CreateProjectParam2 = name
 	s.CreateProjectCalls++
-	return nil
 }
 
-func TestSelectCmd(t *testing.T) {
-	t.Parallel()
-
-	t.Run("select project without name", func(t *testing.T) {
-		t.Parallel()
-
-		// given
-		svc := mockService()
-		cli := cli.NewCli(svc)
-		args := []string{"select"}
-
-		// when
-		cli.Run(args)
-
-		// then
-		if svc.SelectAndOpenProjectCalls != 1 {
-			t.Errorf("SelectAndOpenProject should be called once")
-		}
-
-		if svc.SelectAndOpenProjectParam1 != "" {
-			t.Errorf("SelectAndOpenProject should be called with empty string")
-		}
-	})
-
-	t.Run("select project with name", func(t *testing.T) {
-		t.Parallel()
-
-		// given
-		svc := mockService()
-		cli := cli.NewCli(svc)
-		args := []string{"select", "test-name"}
-
-		// when
-		cli.Run(args)
-
-		// then
-		if svc.SelectAndOpenProjectCalls != 1 {
-			t.Errorf("SelectAndOpenProject should be called once")
-		}
-
-		if svc.SelectAndOpenProjectParam1 != "test-name" {
-			t.Errorf("SelectAndOpenProject should be called with test-name")
-		}
-	})
+func (s *MockService) DeleteProject(name string) {
+	s.DeleteProjectParam1 = name
+	s.DeleteProjectCalls++
 }
 
-func TestCreateCmd(t *testing.T) {
-	t.Parallel()
+func (s *MockService) EditProject(name string) {
+	s.EditProjectParam1 = name
+	s.EditProjectCalls++
+}
 
-	t.Run("create project without name", func(t *testing.T) {
-		t.Parallel()
+func TestRun(t *testing.T) {
+	t.Run("select command", func(t *testing.T) {
+		t.Run("select project", func(t *testing.T) {
+			variants := [][]string{
+				{"select"},
+				{"s"},
+				{},
+			}
 
-		// given
-		svc := mockService()
-		cli := cli.NewCli(svc)
-		args := []string{"create"}
+			for _, args := range variants {
+				// given
+				svc := mockService()
+				cli := cli.NewCli(svc)
 
-		// when
-		cli.Run(args)
+				// when
+				cli.Run(args)
 
-		// then
-		if svc.CreateProjectCalls != 1 {
-			t.Errorf("CreateProject should be called once")
-		}
-		if svc.CreateProjectParam2 != svc.CreateProjectParam1 {
-			t.Errorf("Name should default to current working directory")
-		}
+				// then
+				if svc.SelectAndOpenProjectCalls != 1 {
+					t.Errorf("SelectAndOpenProject should be called once")
+				}
+				if svc.SelectAndOpenProjectParam1 != "" {
+					t.Errorf("SelectAndOpenProject should be called with empty string")
+				}
+			}
+		})
+
+		t.Run("select project with name", func(t *testing.T) {
+			name := "foobar"
+			variants := [][]string{
+				{"select", name},
+				{"s", name},
+			}
+
+			for _, args := range variants {
+				// given
+				svc := mockService()
+				cli := cli.NewCli(svc)
+
+				// when
+				cli.Run(args)
+
+				// then
+				if svc.SelectAndOpenProjectCalls != 1 {
+					t.Errorf("SelectAndOpenProject should be called once")
+				}
+
+				if svc.SelectAndOpenProjectParam1 != name {
+					t.Errorf("SelectAndOpenProject should be called with %s", name)
+				}
+			}
+		})
 	})
 
-	t.Run("create project with name", func(t *testing.T) {
-		t.Parallel()
-
-		// given
-		svc := mockService()
-		cli := cli.NewCli(svc)
-		args := []string{"create", "test-name"}
-
-		// when
-		cli.Run(args)
-
-		// then
-		if svc.CreateProjectCalls != 1 {
-			t.Errorf("CreateProject should be called once")
+	t.Run("create command", func(t *testing.T) {
+		variants := [][]string{
+			{"create"},
+			{"c"},
+			{"a"},
+			{"add"},
+			{"append"},
+			{"new"},
 		}
-		if svc.CreateProjectParam2 != "test-name" {
-			t.Errorf("CreateProject should be called with test-name")
-		}
+
+		t.Run("create project", func(t *testing.T) {
+			for _, args := range variants {
+				// given
+				svc := mockService()
+				cli := cli.NewCli(svc)
+
+				// when
+				cli.Run(args)
+
+				// then
+				if svc.CreateProjectCalls != 1 {
+					t.Errorf("CreateProject should be called once")
+				}
+
+				if svc.CreateProjectParam2 != svc.CreateProjectParam1 {
+					t.Errorf("Name should default to current working directory")
+				}
+			}
+		})
+
+		t.Run("create project with name", func(t *testing.T) {
+			name := "foobar"
+
+			for _, args := range variants {
+				// given
+				svc := mockService()
+				cli := cli.NewCli(svc)
+
+				// when
+				cli.Run(append(args, name))
+
+				// then
+				if svc.CreateProjectCalls != 1 {
+					t.Errorf("CreateProject should be called once")
+				}
+
+				if svc.CreateProjectParam2 != name {
+					t.Errorf("Name should be %s", name)
+				}
+			}
+		})
+
+		t.Run("create project with name and cwd", func(t *testing.T) {
+			name := "foobar"
+			cwd := "/home/test"
+
+			for _, args := range variants {
+				// given
+				svc := mockService()
+				cli := cli.NewCli(svc)
+
+				// when
+				cli.Run(append(args, name, cwd))
+
+				// then
+				if svc.CreateProjectCalls != 1 {
+					t.Errorf("CreateProject should be called once")
+				}
+
+				if svc.CreateProjectParam1 != cwd {
+					t.Errorf("Cwd should be %s", cwd)
+				}
+
+				if svc.CreateProjectParam2 != name {
+					t.Errorf("Name should be %s", name)
+				}
+			}
+		})
 	})
 
-	t.Run("create project wih name and cwd", func(t *testing.T) {
-		t.Parallel()
-		// given
-		svc := mockService()
-		cli := cli.NewCli(svc)
-		args := []string{"create", "test-name", "/home/test"}
-
-		// when
-		cli.Run(args)
-
-		// then
-		if svc.CreateProjectCalls != 1 {
-			t.Errorf("CreateProject should be called once")
+	t.Run("delete command", func(t *testing.T) {
+		variants := [][]string{
+			{"delete"},
+			{"d"},
 		}
 
-		if svc.CreateProjectParam1 != "/home/test" {
-			t.Errorf("CreateProject should be called with /home/test")
+		t.Run("delete project", func(t *testing.T) {
+			for _, args := range variants {
+				// given
+				svc := mockService()
+				cli := cli.NewCli(svc)
+
+				// when
+				cli.Run(args)
+
+				// then
+				if svc.DeleteProjectCalls != 1 {
+					t.Errorf("DeleteProject should be called once")
+				}
+
+				if svc.DeleteProjectParam1 != "" {
+					t.Errorf("DeleteProject should be called with empty string")
+				}
+			}
+		})
+
+		t.Run("delete project with name", func(t *testing.T) {
+			name := "foobar"
+
+			for _, args := range variants {
+				// given
+				svc := mockService()
+				cli := cli.NewCli(svc)
+
+				// when
+				cli.Run(append(args, name))
+
+				// then
+				if svc.DeleteProjectCalls != 1 {
+					t.Errorf("DeleteProject should be called once")
+				}
+
+				if svc.DeleteProjectParam1 != name {
+					t.Errorf("DeleteProject should be called with %s", name)
+				}
+			}
+		})
+	})
+
+	t.Run("edit command", func(t *testing.T) {
+		variants := [][]string{
+			{"edit"},
+			{"e"},
 		}
 
-		if svc.CreateProjectParam2 != "test-name" {
-			t.Errorf("CreateProject should be called with test-name")
-		}
+		t.Run("edit project", func(t *testing.T) {
+			for _, args := range variants {
+				// given
+				svc := mockService()
+				cli := cli.NewCli(svc)
+
+				// when
+				cli.Run(args)
+
+				// then
+				if svc.EditProjectCalls != 1 {
+					t.Errorf("EditProject should be called once")
+				}
+
+				if svc.EditProjectParam1 != "" {
+					t.Errorf("EditProject should be called with empty string")
+				}
+			}
+		})
+
+		t.Run("delete project with name", func(t *testing.T) {
+			name := "foobar"
+
+			for _, args := range variants {
+				// given
+				svc := mockService()
+				cli := cli.NewCli(svc)
+
+				// when
+				cli.Run(append(args, name))
+
+				// then
+				if svc.EditProjectCalls != 1 {
+					t.Errorf("EditProject should be called once")
+				}
+
+				if svc.EditProjectParam1 != name {
+					t.Errorf("EditProject should be called with %s", name)
+				}
+			}
+		})
 	})
 }
