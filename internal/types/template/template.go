828d9fe46b7a34387935667b65b24fdc12f30203
Dominik <wezik@users.noreply.github.com>
2025-06-21 14:46:08 +0200

    Introduce defaults for windows and panes

--- internal/types/template/template.go
+++ internal/types/template/template.go
@@ -1,7 +1,9 @@
 package template
 
 import (
+	"strconv"
 	"thop/internal/types/command"
+	"thop/internal/types/pane"
 	"thop/internal/types/window"
 )
 
@@ -15,6 +17,42 @@ type Template struct {
 	Name         Name              `yaml:"name,omitempty"`
 	Root         Root              `yaml:"root"`
 	Commands     []command.Command `yaml:"run,omitempty"`
-	Windows      []window.Window   `yaml:"windows"`
+	Windows      []window.Window   `yaml:"windows, omitempty"`
 	ActiveWindow ActiveWindow      `yaml:"active_window,omitempty"`
+}
+
+// Will set default values for missing fields
+func (t *Template) WithDefaults() Template {
+	newTemplate := *t
+
+	if newTemplate.Windows == nil || len(newTemplate.Windows) == 0 {
+		newTemplate.Windows = []window.Window{
+			{
+				Name: "main",
+				Panes: []pane.Pane{
+					{
+						Name: "main",
+					},
+				},
+			},
+		}
+	}
+
+	for i := range newTemplate.Windows {
+		win := &newTemplate.Windows[i]
+
+		if win.Name == "" {
+			win.Name = window.Name("window" + strconv.Itoa(i))
+		}
+		
+		if win.Panes == nil || len(win.Panes) == 0 {
+			win.Panes = []pane.Pane{
+				{
+					Name: pane.Name("pane" + strconv.Itoa(i)),
+				},
+			}
+		}
+	}
+
+	return newTemplate
 }
